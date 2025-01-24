175c07b9de9cd07a9465f673b8342eea93d198d4
Joonas Reinikka <joonas.reinikka@gmail.com>
2025-01-24 22:33:27 +0200

    Update item to add scene

--- scripts/item.gd
+++ scripts/item.gd
@@ -6,3 +6,8 @@ class_name Item
 @export var ui_icon: Texture
 @export var stackable: bool
 @export var stack_size: int # <= 0 means infinity
+
+# Link to the corresponding pickable item scene, if you need to drop the item back on the floor
+@export_file("*.tscn") var pickable_item_scene
+
+var _pickable_item_scene: PackedScene
