781f293b5ed5c219861fa1bb81e132a8a04e7c06
Joni Louhela <louhela.joni@gmail.com>
2025-01-24 22:45:02 +0200

    Add action for buying yeast

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,9 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
+enum Action {NONE, BUY_YEAST}
+
+var current_action: Action = Action.NONE
 
 func _physics_process(delta: float) -> void:
 	var h_direction := Input.get_axis("move_left", "move_right")
@@ -35,3 +38,14 @@ func _update_action_indicator():
 	elif velocity.y < 0:
 		action_indicator.position.y = -28
 		action_indicator.position.x = 0
+
+
+func _on_action_indicator_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	if len(area.get_groups()) == 0:
+		printerr("grouppi puuttuu action target arealta")
+	var group = area.get_groups()[0]
+	if group == "yeast_buy_zone":
+		current_action = Action.BUY_YEAST
+		
+func _on_action_indicator_area_2d_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	current_action = Action.NONE
