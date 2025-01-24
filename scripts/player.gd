97ebd5a467e523038adddd79376aaa7144c6c0e8
Joni Louhela <louhela.joni@gmail.com>
2025-01-24 19:42:40 +0200

    add player

---
+++ scripts/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	var h_direction := Input.get_axis("move_left", "move_right")
+	if h_direction:
+		velocity.x = h_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var v_direction := Input.get_axis("move_up", "move_down")
+	if v_direction:
+		velocity.y = v_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
