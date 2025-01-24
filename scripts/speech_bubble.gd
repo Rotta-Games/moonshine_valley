3e42840d2e369a9cb9c3874830cad227ac3a9ab3
Joni Louhela <louhela.joni@gmail.com>
2025-01-24 21:35:23 +0200

    shopkeeper talks

--- scripts/speech_bubble.gd
+++ scripts/speech_bubble.gd
@@ -1,11 +1,21 @@
 extends Node2D
+class_name SpeechBubble
 
+var text: String = ""
+@onready var label : Label = $"BubbleSprite/TextLabel" 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	label.visible_ratio = 0
+	
+func play_text(text: String):
+	
+	if self.visible:
+		return
+		
+	self.visible = true
+	label.text = text
+	var tween = get_tree().create_tween()
+	tween.tween_property(label, "visible_ratio", 1, 1)
+	await get_tree().create_timer(2).timeout
+	label.visible_ratio = 0
+	self.visible = false
