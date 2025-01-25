376ac820d825c02264b152985d6fba4b378dbb88
Joonas Reinikka <joonas.reinikka@gmail.com>
2025-01-25 22:00:36 +0200

    Fix timer

--- scripts/help_text.gd
+++ scripts/help_text.gd
@@ -4,6 +4,8 @@ var text: String = ""
 
 @onready var label : Label = $"TextLabel" 
 func play_text(sent_text: String):
+	$Timer.start()
+	
 	if self.visible && text == sent_text:
 		return
 	label.visible_ratio = 0
@@ -13,8 +15,6 @@ func play_text(sent_text: String):
 	var tween = get_tree().create_tween()
 	tween.tween_property(label, "visible_ratio", 1, 1)
 	await get_tree().create_timer(2).timeout
-	label.visible_ratio = 0
-	self.visible = false
 
 
 func _on_timer_timeout():
