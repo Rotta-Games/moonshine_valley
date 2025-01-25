f67b0d41d1a928c6ee79b74baed2e27468306147
Pete Sevander <pete.sevander@gmail.com>
2025-01-25 17:25:31 +0000

    Selling area and bucket fixes

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var _bucket_fill_view: Control = $"WindowCanvasLayer/BucketFillView"
 
+signal bucket_sold(bucket_state: Bucket.State)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -17,3 +19,11 @@ func _on_player_bucket_inspected(bucket: Bucket) -> void:
 func _on_bucket_fill_view_closed() -> void:
 	get_tree().paused = false
 	_bucket_fill_view.hide()
+
+
+func _on_bucket_entered_selling_area(body: Node2D) -> void:
+	if body is Bucket:
+		print("VOITIT PELIN :D")
+		bucket_sold.emit(body.state)
+		body.queue_free()
+
