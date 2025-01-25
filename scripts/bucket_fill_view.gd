a1e2bdf7b0110d1f5b014d33cbe7230d968c4606
Joni Louhela <louhela.joni@gmail.com>
2025-01-25 15:47:24 +0200

    Bucket texture (wrong one tho)

--- scripts/bucket_fill_view.gd
+++ scripts/bucket_fill_view.gd
@@ -7,15 +7,6 @@ extends CanvasLayer
 
 var _animation_playing: bool = false
 var _rng : RandomNumberGenerator = RandomNumberGenerator.new()
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("player_action"):
-		add_sugar()
 		
 func _reset_ingredient(sprite: TextureRect):
 	sprite.position = _ingredient_start_location.position
