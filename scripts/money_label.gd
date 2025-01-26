7de25d2f153278f2978eb2671733da7bcda6fc85
Pete Sevander <pete.sevander@gmail.com>
2025-01-26 11:15:03 +0000

    initial gh pages

--- scripts/money_label.gd
+++
@@ -1,4 +0,0 @@
-extends Control
-
-func _update_text(amount: int) -> void:
-	self.text = str(amount / 100.0) + "€"
--- scripts/pickable_item.gd
+++
@@ -1,5 +0,0 @@
-@tool
-extends Node2D
-class_name PickableItem
-
-@export var item: Item
